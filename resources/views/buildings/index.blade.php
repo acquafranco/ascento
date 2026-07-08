8cea2a5ce918e01fc4c6f7a33e27efb8a73e1cf1
Franco Acqua <francohares@gmail.com>
2026-07-08 22:41:04 +0000

    cambio en la vista index buildings

--- resources/views/buildings/index.blade.php
+++ resources/views/buildings/index.blade.php
@@ -129,7 +129,19 @@
 
                                 </div>
 
-                                <form method="POST" action="{{ route('building-check.done', $building) }}">
+                                <form
+                                    method="POST"
+                                    action="{{ route('building-check.done', $building) }}"
+                                    onsubmit="return confirm(
+                                'Este mantenimiento ya posee un remito asociado.
+
+                                Si lo desmarcás, el remito NO será eliminado.
+
+                                El edificio volverá a aparecer como pendiente y podrías generar un remito duplicado.
+
+                                ¿Deseás continuar?'
+                                )"
+                                >
                                     @csrf
 
                                     <button class="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-xl font-semibold">
