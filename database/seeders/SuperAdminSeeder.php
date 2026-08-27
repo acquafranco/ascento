ff28dec92154dc11b0631a48a94a0f568ce9d8f2
Franco Acqua <francoacqua@xlr8.local>
2026-08-27 15:04:46 -0300

    cambio de seeders

--- database/seeders/SuperAdminSeeder.php
+++ database/seeders/SuperAdminSeeder.php
@@ -2,37 +2,23 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
 class SuperAdminSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         User::updateOrCreate(
-
-        ['email' => 'franco@ascento.online'],
-
-        [
-
-            'name' => 'Franco Acqua',
-
-            'password' => Hash::make(env('SUPER_ADMIN_PASSWORD')),
-
-            'role' => 'admin',
-
-            'is_super_admin' => true,
-
-            'company_id' => null,
-
-        ]
-
-    );
+            ['email' => 'franco@ascento.online'],
+            [
+                'name' => 'Franco Acqua',
+                'password' => Hash::make(env('SUPER_ADMIN_PASSWORD')),
+                'role' => 'admin',
+                'is_super_admin' => true,
+                'company_id' => null,
+            ]
+        );
     }
 }
-// PRUEBA GIT
